f40dcdb1db4f9f6972e18420cfdde83b8a122919
kekelele996 <kekelele996@users.noreply.github.com>
2026-08-18 21:40:27 +0800

    fix: 005

--- approval_tx_service.go
+++ approval_tx_service.go
@@ -3,7 +3,11 @@ package freight
 func ApproveSettlement(r *ApprovalRepository, e *ApprovalEffects) error {
 	current := r.Get()
 	candidate := approvalCandidate(&current)
+	if err := r.save(*candidate); err != nil {
+		rollbackApprovalCheckpoint(e)
+		return err
+	}
 	advanceApprovalCheckpoint(e, *candidate)
 	e.Events = append(e.Events, "approved:"+candidate.ID)
-	return r.save(*candidate)
+	return nil
 }
--- approval_tx_store.go
+++ approval_tx_store.go
@@ -3,9 +3,9 @@ package freight
 func (r *ApprovalRepository) save(v ApprovalRecord) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	r.record = v
 	if r.Conflict {
 		return ErrConflict
 	}
+	r.record = v
 	return nil
 }
