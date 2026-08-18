bc8fdc2d4cf871f59075cf21f6cd1b117e182276
gaobo <304590126@qq.com>
2026-08-18 17:30:00 +0800

    gold: freight-escrow-001

--- request_lease_abort.go
+++ request_lease_abort.go
@@ -3,6 +3,14 @@ package freight
 func (s *RequestLeaseStore) AbortLease(tenant, key, leaseID string) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	delete(s.records, key)
+	k := leaseStorageKey(tenant, key)
+	v, ok := s.records[k]
+	if !ok {
+		return ErrNotFound
+	}
+	if v.LeaseID != leaseID || v.State != "reserved" {
+		return leaseConflict()
+	}
+	delete(s.records, k)
 	return nil
 }
--- request_lease_reserve.go
+++ request_lease_reserve.go
@@ -1,9 +1,12 @@
 package freight
 
+import "fmt"
+
 func (s *RequestLeaseStore) ReserveLease(tenant, key, fp string) (RequestLease, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	k := leaseStorageKey(tenant, key)
+	scope := leaseTenantScope(tenant)
+	k := leaseStorageKey(scope, key)
 	if v, ok := s.records[k]; ok {
 		if v.Fingerprint != fp {
 			return RequestLease{}, leaseConflict()
@@ -11,7 +14,7 @@ func (s *RequestLeaseStore) ReserveLease(tenant, key, fp string) (RequestLease, 
 		return v, nil
 	}
 	s.sequence++
-	v := RequestLease{ClientKey: key, Fingerprint: fp, LeaseID: key, State: "reserved"}
+	v := RequestLease{Scope: scope, ClientKey: key, Fingerprint: fp, LeaseID: fmt.Sprintf("%s/%s/%d", scope, key, s.sequence), State: "reserved"}
 	s.records[k] = v
 	return v, nil
 }
